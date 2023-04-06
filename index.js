3aa05a20d56e7c5af97f73acd84387c87c70ea8b
Saurabh Tripathi <saurabh.umesh.tripathi@gmail.com>
2023-04-06 18:17:23 +0530

    updated/added files

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const posts = [
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
-        avatar: "images/avatar-ducreux.js",
+        avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152
